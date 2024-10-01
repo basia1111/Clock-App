5faf81a1883a3f170797cc523cbc6f3129c3230e
basia1111 <basia.zygilewicz@gmail.com>
2024-10-01 15:28:52 +0200

    fix clock hands

--- src/components/AnalogClock.jsx
+++ src/components/AnalogClock.jsx
@@ -14,13 +14,13 @@ function AnalogClock(){
         <div className ="w-full max-w-[270px] aspect-square rounded-full border-4 p-20 border-dark-purple  dark:border-white relative">
 
             <div className = "absolute  z-10 bg-dark-purple  dark:bg-white w-[3px] h-[25%] origin-bottom bottom-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 "
-                style={{transform: `rotate(${hours * 30}deg)`}}
+                style={{transform: `rotate(${hours * 30}deg)`, transition: hours === 0 ? 'none' : 'transform 0.05s linear',}}
             ></div>
             <div className = "absolute  z-20 bg-dark-purple  dark:bg-white w-[3px] h-[35%] origin-bottom bottom-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 "
-                style={{transform: `rotate(${min * 6}deg)`}}
+                style={{transform: `rotate(${min * 6}deg)`, transition: min === 0 ? 'none' : 'transform 0.05s linear',}}
             ></div>
             <div className = "absolute z-30 bg-dark-purple  dark:bg-white w-[2px] h-[43%] origin-bottom bottom-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 " 
-                style={{transform: `rotate(${sec * 6}deg)`}}
+                style={{transform: `rotate(${sec * 6}deg)`, transition: sec === 0 ? 'none' : 'transform 0.05s linear',}}
             ></div>
             <div className = "absolute z-40 bg-dark-purple  dark:bg-white  w-[4%] h-[4%] origin-bottom top-[50%] left-[50%]  transform -translate-x-1/2 -translate-y-1/2 rounded-full "></div>
 
